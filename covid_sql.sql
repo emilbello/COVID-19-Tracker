47d239153d864d0867b467dbf7ab4a44f4316087
mmead <mmeadx@gmail.com>
2020-10-17 16:27:54 -0500

    Working TimeSeries and covidsql

--- covid_sql.sql
+++ covid_sql.sql
@@ -1,26 +1,26 @@
 CREATE TABLE covid_data (
-date date,
-state TEXT,
-state_abbr TEXT,
-population INT,
-positive FLOAT,
-positive_increase INT,
-death FLOAT,
-death_increase INT,
-hospitalized_currently FLOAT,
-hospitalized_increase INT,
-hospitalized_cumulative FLOAT,
-in_icu_currently FLOAT,
-in_icu_cumulative FLOAT,
-on_ventilator_currently FLOAT,
-on_ventilator_cumulative FLOAT,
-data_quality_grade TEXT,
-positive_per_100k FLOAT,
-death_per_100k FLOAT
+    date date,
+    state TEXT PRIMARY KEY,
+    state_abbr TEXT,
+    population INT,
+    positive FLOAT,
+    positive_increase INT,
+    death FLOAT,
+    death_increase INT,
+    hospitalized_currently FLOAT,
+    hospitalized_increase INT,
+    hospitalized_cumulative FLOAT,
+    in_icu_currently FLOAT,
+    in_icu_cumulative FLOAT,
+    on_ventilator_currently FLOAT,
+    on_ventilator_cumulative FLOAT,
+    data_quality_grade TEXT,
+    positive_per_100k FLOAT,
+    death_per_100k FLOAT
 );
 
 CREATE TABLE covid_hist (
-    date date,
+    date date PRIMARY KEY,
     positive INT,
     negative INT,
     death FLOAT,
@@ -30,6 +30,7 @@ CREATE TABLE covid_hist (
 );
 
 CREATE TABLE covid_rolling (
+    index INT PRIMARY KEY,
     state TEXT,
     date date,
     positive FLOAT,
